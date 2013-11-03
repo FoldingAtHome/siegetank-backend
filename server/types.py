5eb1696e0fe01b0a5ec664100d8ada50a7dc239a
Yutong Zhao <yutong.zhao@stanford.edu>
2013-11-03 11:24:35 -0800

    Added user sqlalchemy class

--- server/types.py
+++ server/types.py
@@ -3,13 +3,13 @@ from sqlalchemy import Column, Integer, String, ForeignKey
 from sqlalchemy.types import LargeBinary
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy import create_engine
+import uuid
 Base = declarative_base()
 
 class Project(Base):
     __tablename__ = 'projects'
     # required
     uuid = Column(String, nullable=False, primary_key=True)
-    owner = Column(String, nullable=False)
     description = Column(String, nullable=False)
     system = Column(LargeBinary, nullable=False)
     integrator = Column(LargeBinary, nullable=False)
@@ -18,13 +18,16 @@ class Project(Base):
     frame_format = Column(String, nullable=False)
     precision = Column(Integer, nullable=False)
 
-    def __init__(self, uuid, owner, description, system, integrator, 
+    # relationships
+    owner = Column(String, ForeignKey('users.username'), nullable=False)
+    user = relationship("User", backref=backref('projects', order_by=uuid))
+
+    def __init__(self, uuid, description, system, integrator, 
                 steps_per_frame=50000,
                 frame_format='xtc', 
                 precision=3):
 
         self.uuid = uuid
-        self.owner = owner
         self.description = description
         self.system = system
         self.integrator = integrator
@@ -36,8 +39,9 @@ class Stream(Base):
     __tablename__ = 'streams'
     uuid = Column(String, nullable=False, primary_key=True)
     state = Column(LargeBinary, nullable=False)
-
     frames = Column(Integer)
+
+    # relationships
     project_uuid = Column(String, ForeignKey('projects.uuid'), nullable=False)
     project = relationship("Project",backref=backref('streams', order_by=uuid))
     
@@ -45,6 +49,19 @@ class Stream(Base):
         self.uuid = uuid
         self.state = state
         self.frames = 0
+
+class User(Base):
+    __tablename__ = 'users'
+    username = Column(String, nullable=False, primary_key = True)
+    password = Column(String, nullable=False)
+    email = Column(String, nullable=False)
+    token = Column(String, nullable=False)
+
+    def __init__(self, username, password, email):
+        self.username = username
+        self.password = password
+        self.email = email
+        self.token = str(uuid.uuid4())
 
 def initialize():
     engine = create_engine('postgresql://localhost/sandbox2', echo=True)
