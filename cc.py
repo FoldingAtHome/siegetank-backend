6bb35deb797bf636a083a7f80f4425785217ebd8
Yutong Zhao <yutong.zhao@stanford.edu>
2013-11-17 16:22:44 -0800

    Begin dumping Flask for Tornado

--- cc.py
+++ cc.py
@@ -1,63 +1,69 @@
-from flask import Flask, Response, request, abort, jsonify, make_response
-import redis
-import uuid
+from datetime import date
+import tornado.escape
+import tornado.ioloop
+import tornado.web
+import tornado.auth
+
 import hashlib
+import json
+import os
 
-app = Flask(__name__)
-redis_cli = redis.Redis()
+import redis
 
-# Redis always serves as the communication interface
-# User, project, data are stored in the underlying database()
+cc_redis = redis.Redis(host='localhost', port=6379)
 
-# project:uuid - maps to IP of WS holding the project
-@app.route('/st/add_user', methods=['POST'])
-def add_user():
-    if not 'username' in request.json \
-        or not 'password' in request.json \
-        or not 'email' in request.json:
-        abort(401)
+class TargetHandler(tornado.web.RequestHandler):
+    def post(self):
+        ''' PGI: Post a new target '''
+        content = json.loads(self.request.body)
+        try:
+            system = content['system']
+            system_sha = hashlib.sha256(system).hexdigest()
+            open('./files/'+system_sha,'w').write(system)
+            integrator = content['integrator']
+            integrator_sha = hashlib.sha256(integrator).hexdigest()
+            open('./files/'+integrator_sha,'w').write(integrator)
+            description = content['description']
+            description_sha = hashlib.sha256(description).hexdigest()
+            creation_time = cc_redis.time()[0]
+            target_id = hashlib.sha256(system_sha+integrator_sha+\
+                                       description_sha+str(creation_time)).hexdigest()
+            owner = 'yutong'
 
-    username = request.json['username']
-    password = request.json['password']
-    email = request.json['email']
+            print target_id
 
-    if len(username) > 10:
-        return 'username longer than 10 characters', 401
+            # store data into redis
+            cc_redis.hset(target_id,'system',system_sha)
+            cc_redis.hset(target_id,'integrator',integrator_sha)
+            cc_redis.hset(target_id,'description',description)
+            cc_redis.hset(target_id,'date',creation_time)
+            cc_redis.hset(target_id,'owner',owner)
 
-    if username.isalnum() == False:
-        return 'username not alpha numeric', 401
+            # add id to the owner's list of targets
+            cc_redis.sadd(owner+':targets',target_id)
 
-    # see if user already exists in db
-    if redis_cli.sismember('users', username) == True:
-        return 'user exists', 401
-    else:
-        redis_cli.sadd('users', username)
-        redis_cli.hset('user:'+username, 'username', username)
-        redis_cli.hset('user:'+username, 'password', password)
-        redis_cli.hset('user:'+username, 'email', email)
-        redis_cli.hset('user:'+username, 'token', hashlib.sha256(username+password).hexdigest())
+        except Exception as e:
+            print str(e)
+            return self.write('bad request')
 
-    return 'Success', 200
+    def get(self):
+        ''' Get a list of stream IDs and its properties '''
+        print self.request.body
 
-@app.route('/st/auth', methods=['GET'])
-def authenticate():
-    if not request.json:
-        abort(400)
-    if not 'username' in request.json or not 'password' in request.json:
-            abort(400)
+class StreamHandler(tornado.web.RequestHandler):
+    def post(self):
+        ''' PGI: Add new streams to an existing target. The input must be compressed
+            state.xml files encoded as base64 '''
+        print self.request.body
 
-    username = request.json['username']
-    password = request.json['password']
-
-    if not redis_cli.sismember('users', username):
-        return 'Bad username', 401
-
-    real_pw, token = redis_cli.hmget('user:'+username, ['password', 'token'])
-
-    if password != real_pw:
-        return 'Bad password', 401
-
-    return jsonify( {'token': str(token) } ) 
-
+application = tornado.web.Application([
+    (r'/target', TargetHandler),
+    (r'/stream', StreamHandler)
+])
+ 
 if __name__ == "__main__":
-    app.run(debug = True, host='0.0.0.0')
+    cc_redis.flushdb()
+    application.listen(8888)
+    if not os.path.exists('files'):
+        os.makedirs('files')
+    tornado.ioloop.IOLoop.instance().start()
