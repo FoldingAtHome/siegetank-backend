ab621b2dbbe6c9cab828c76f9ad69b3356ebfb45
Yutong Zhao <yutong.zhao@stanford.edu>
2014-02-19 21:55:55 -0800

    Add setup.py

---
+++ setup.py
@@ -0,0 +1,10 @@
+from distutils.core import setup
+
+setup(name='Backend',
+      version='0.1',
+      description='Siegetank Backend',
+      author='Yutong Zhao',
+      author_email='proteneer@gmail.com',
+      url='http://www.proteneer.com',
+      packages=['server'],
+      )
