2089a7e04624d7eb835d1aecc19b0522a67876df
Yutong Zhao <yutong.zhao@stanford.edu>
2014-02-27 19:07:45 -0800

    wip

--- openmm.py
+++ openmm.py
@@ -1,4 +1,5 @@
 import requests
+import json
 import simtk.openmm
 
 
@@ -10,39 +11,69 @@ class Stream():
 
 class Target():
     # create a new target on the backend
-    def __init__(self, ):
-        self.id = id
+    def __init__(self, id, cc_uri):
+        self.cc_uri = cc_uri
+        self._id = id
+        self._description = None
+        self._steps_per_frame = None
+        self._creation_date = None
+        self._allowed_ws = None
+        self._engine = None
+        self._engine_versions = None
 
     def add_stream(files):
         print('hehe')
 
+    def reload(self):
+        reply = requests.get(uri+'/targets/info/'+self.id)
+        if(reply.status_code != 200):
+            raise Exception("Failed to fill target internals")
+
+    @property
+    def id(self):
+        return self._id
+
     @property
     def streams(self):
-        return self.streams
+        if not self._streams:
+            self.reload()
+        return self._streams
 
     @property
     def description(self):
-        pass
+        if not self._description:
+            self.reload()
+        return self._description
 
     @property
     def steps_per_frame(self):
-        pass
+        if not self._steps_per_frame:
+            self.reload()
+        return self._steps_per_frame
 
     @property
     def creation_date(self):
-        pass
+        if not self._creation_date:
+            self.reload()
+        return self._creation_date
 
     @property
     def allowed_ws(self):
-        pass
+        if not self._allowed_ws:
+            self.reload()
+        return self._allowed_ws
 
     @property
     def engine(self):
-        pass
+        if not self._engine:
+            self.reload()
+        return self._engine
 
     @property
     def engine_versions(self):
-        pass
+        if not self._engine_versions:
+            self.reload()
+        return self._engine_versions
 
     @classmethod
     def load_from_id(id):
