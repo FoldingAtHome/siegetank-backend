39c5c7f20faac02b88c0c7f7f8827e16df3eacb6
Yutong Zhao <yutong.zhao@stanford.edu>
2014-03-02 18:40:48 -0800

    pypi migration

--- setup.py
+++ setup.py
@@ -1,9 +1,13 @@
+# we do not distribute the server code via pypi, ever. So instead, we use the
+# pypi file here to distribute the siegetank API code.
+
 from distutils.core import setup
 
-setup(name='Backend',
+setup(name='siegetank',
       version='0.1',
-      description='Siegetank Backend',
+      description='Backend Python driver',
       author='Yutong Zhao',
       author_email='proteneer@gmail.com',
       url='http://www.proteneer.com',
+      packages=['siegetank']
       )
