69a0f864d8b76cf317cbf452ebbde08d761d7ee0
Yutong Zhao <yutong.zhao@stanford.edu>
2014-04-20 10:10:19 -0700

    Load CAs from OpenSSL

--- core/tests/test_poco.cpp
+++ core/tests/test_poco.cpp
@@ -18,7 +18,7 @@ int main() {
     try {
         Poco::Net::Context::Ptr ctxt = new Poco::Net::Context(
             Poco::Net::Context::CLIENT_USE, "", Poco::Net::Context::VERIFY_STRICT,
-            9, false);
+            9, true);
         Poco::URI uri("https://www.google.com");
         Poco::Net::HTTPSClientSession session(uri.getHost(),
                                               uri.getPort(), ctxt);
