044c6d2859367ceaf213fbbaa615edde077a3631
Yutong Zhao <yutong.zhao@stanford.edu>
2014-06-18 13:45:26 -0700

    Fix

--- scripts/bootstrap_aws_cc.sh
+++ scripts/bootstrap_aws_cc.sh
@@ -1,6 +1,6 @@
 # !/bin/bash
 
-sudo apt-get install git python3 python3-pip libffi-dev python3-dev;
+sudo apt-get install python3 python3-pip libffi-dev python3-dev;
 sudo pip3 install -r requirements.txt;
 sudo iptables -t nat -A PREROUTING -p tcp --dport 80 -j REDIRECT --to-port 8980;
 cd redis; make -j4; cd ..;
