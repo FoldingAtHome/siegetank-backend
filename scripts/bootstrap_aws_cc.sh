d9e21234e70d59c58442ae9763d3de166b2213f6
Yutong Zhao <yutong.zhao@stanford.edu>
2014-06-18 14:23:49 -0700

    add iptables save

--- scripts/bootstrap_aws_cc.sh
+++ scripts/bootstrap_aws_cc.sh
@@ -3,6 +3,7 @@
 sudo apt-get install python3 python3-pip libffi-dev python3-dev;
 sudo pip3 install -r requirements.txt;
 sudo iptables -t nat -A PREROUTING -p tcp --dport 80 -j REDIRECT --to-port 8980;
+sudo iptables-save;
 cd redis; make -j4; cd ..;
 sudo sh -c "echo ' * hard nofile 64000 ' >> /etc/security/limits.conf";
 sudo sh -c "echo ' * soft nofile 64000 ' >> /etc/security/limits.conf";
