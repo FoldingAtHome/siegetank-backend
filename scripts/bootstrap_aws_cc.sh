ce8dade26f26a8baff482bfe2f4eb386e8b8774e
Yutong Zhao <yutong.zhao@stanford.edu>
2014-06-18 13:47:15 -0700

    UPdate

--- scripts/bootstrap_aws_cc.sh
+++ scripts/bootstrap_aws_cc.sh
@@ -4,6 +4,6 @@ sudo apt-get install python3 python3-pip libffi-dev python3-dev;
 sudo pip3 install -r requirements.txt;
 sudo iptables -t nat -A PREROUTING -p tcp --dport 80 -j REDIRECT --to-port 8980;
 cd redis; make -j4; cd ..;
-sudo echo " * hard nofile 64000 " >> limits.conf;
-sudo echo " * soft nofile 64000 " >> limits.conf;
+sudo echo " * hard nofile 64000 " >> /etc/security/limits.conf;
+sudo echo " * soft nofile 64000 " >> /ect/security/limits.conf;
 sudo shutdown -r 0;
