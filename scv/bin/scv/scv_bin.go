6398e3333f48ad2e7bc77cc88f72d40094c56081
Yutong Zhao <proteneer@gmail.com>
2014-12-07 18:05:56 -0500

    Code cleanup

---
+++ scv/bin/scv/scv_bin.go
@@ -0,0 +1,45 @@
+package main
+
+import (
+	"../../src"
+	"encoding/json"
+	"flag"
+	"fmt"
+	"log"
+	"os"
+	"path/filepath"
+	"runtime"
+)
+
+func main() {
+
+	runtime.GOMAXPROCS(5)
+	conf := scv.Configuration{
+		SSL: make(map[string]string),
+	}
+
+	var configFile = flag.String("config", "", "configuration file for the SCV")
+
+	flag.Parse()
+
+	fmt.Println(*configFile)
+	config := ""
+	if *configFile == "" {
+		dir, _ := filepath.Abs(filepath.Dir(os.Args[0]))
+		config = filepath.Join(dir, "scv.json")
+	} else {
+		config = *configFile
+	}
+
+	log.Println("Config file: ", config)
+
+	file, err := os.Open(config)
+	if err != nil {
+		panic("Could not open config file.")
+	}
+	decoder := json.NewDecoder(file)
+	err = decoder.Decode(&conf)
+	app := scv.NewApplication(conf)
+
+	app.Run()
+}
